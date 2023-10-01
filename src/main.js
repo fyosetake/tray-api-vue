970c9d7620a096293b7f2db18024367330632b5f
fyosetake <fyosetake@gmail.com>
2023-10-01 11:42:12 -0300

    Adiciona rota para a raíz da aplicação

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import AutenticarUsuario from './components/AutenticarUsuario.vue';
 import RegistrarUsuario from './components/RegistrarUsuario.vue';
 
 const routes = [
+    { path: '', component: PaginaInicial },
     { path: '/autenticar-usuario', component: AutenticarUsuario },
     { path: '/registrar-usuario', component: RegistrarUsuario },
     { path: '/pagina-inicial', component: PaginaInicial },
