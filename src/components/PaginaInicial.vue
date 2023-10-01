16a6cda1fc77632c7a8c2a986c5ef523ad2b763e
fyosetake <fyosetake@gmail.com>
2023-10-01 10:42:23 -0300

    Cria componente vue 'PaginaInicial'

---
+++ src/components/PaginaInicial.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pagina-inicial">
+    <h1>Seja bem-vindo à esta Aplicação!</h1>
+    <p>Fique à vontade para escolher o item que desejar no menu superior!</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.pagina-inicial {
+  text-align: center; /* Alinha o texto ao centro */
+  margin-top: 100px; /* Define uma margem superior para afastar o conteúdo do topo */
+  font-family: Arial, sans-serif;
+}
+</style>
